c1064019e465b36efcdd09c8527e50feb132848d
pjahn31 <e1156361@u.nus.edu>
2025-02-02 22:29:00 +0800

    Level 3- Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    protected String description;
+    protected Boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); //mark done task with X
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void unmarkAsDone() {
+        this.isDone = false;
+    }
+}
