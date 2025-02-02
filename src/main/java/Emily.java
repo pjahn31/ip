4b984b27dff84604a7e1b32c49894aaf612ff064
pjahn31 <e1156361@u.nus.edu>
2025-02-02 22:28:46 +0800

    Level 3- Emily can mark tasks as done

--- src/main/java/Emily.java
+++ src/main/java/Emily.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Emily {
-    public static ArrayList<String> tasks = new ArrayList<>();
+    public static ArrayList<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -10,34 +10,52 @@ public class Emily {
         System.out.println("What can I do for you?");
 
         while(true) {
-            String task = sc.nextLine().trim();
+            String input = sc.nextLine();
 
-            if(task.equalsIgnoreCase("bye")) {
+            if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if (task.equalsIgnoreCase("list")) {
-                listTasks();
-            } else if (task.startsWith("add ")) {
-                addTask(task.substring(4));
+            } else if (input.equalsIgnoreCase("list")) {
+                printTasks();
+            } else if (input.startsWith("mark ")) {
+                markTaskAsDone(Integer.parseInt(input.substring(5)));
+            } else if (input.startsWith("unmark ")) {
+                unmarkTask(Integer.parseInt(input.substring(7)));
             } else {
-                System.out.println("Sorry I do not know what you are saying. Please type again!");
-            }
-        }
-        sc.close();
-    }
-
-    public static void listTasks() {
-        if (tasks.isEmpty()) {
-            System.out.println("No tasks found.");
-        } else {
-            for (int i = 0; i < tasks.size(); i++) {
-                System.out.println((i + 1) + ". " + tasks.get(i));
+                addTask(input);
             }
         }
     }
 
-    public static void addTask(String task) {
+    public static void addTask(String input) {
+        Task task = new Task(input);
         tasks.add(task);
-        System.out.println("added: " + task);
+        System.out.println("added: " + input);
+    }
+
+    public static void printTasks() {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            Task task = tasks.get(i);
+            System.out.println((i + 1) + ".[" + task.getStatusIcon() + "] " + task.description);
+        }
+    }
+
+    public static void markTaskAsDone(int taskNumber) {
+        if (taskNumber > 0 && taskNumber <= tasks.size()) {
+            Task task = tasks.get(taskNumber - 1);
+            task.markAsDone();
+            System.out.println("Nice! I've marked this task as done: ");
+            System.out.println("  [X] " + task.description);
+        }
+    }
+
+    public static void unmarkTask(int taskNumber) {
+        if (taskNumber > 0 && taskNumber <= tasks.size()) {
+            Task task = tasks.get(taskNumber - 1);
+            task.unmarkAsDone();
+            System.out.println("Ok, I've unmarked this task as not done yet:");
+            System.out.println("  [ ] " + task.description);
+        }
     }
 }
