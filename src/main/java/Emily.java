3221a443e84b21591f6050571e89540fd1d1aeaa
pjahn31 <e1156361@u.nus.edu>
2025-02-02 19:43:21 +0800

    Level 1- Emily echoes

--- src/main/java/Emily.java
+++ src/main/java/Emily.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Emily {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Emily.");
         System.out.println("What can I do for you?");
+        while(true) {
+            String input = sc.nextLine();
+            if(input.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println(input); //Echo back user input
+        }
+        sc.close();
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
