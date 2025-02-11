df82cbe9f4fcba4390ac58a6269371e8c727a1f1
pjahn31 <e1156361@u.nus.edu>
2025-02-12 00:52:17 +0800

    Changes made to Task class and Emily class
    To fit the addition of subclass Todo, Deadline, Event

--- src/main/java/Emily.java
+++ src/main/java/Emily.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Emily {
-    public static ArrayList<Task> tasks = new ArrayList<>();
+    public static final ArrayList<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -21,23 +21,31 @@ public class Emily {
                 markTask(Integer.parseInt(input.substring(5)));
             } else if (input.startsWith("unmark ")) {
                 unmarkTask(Integer.parseInt(input.substring(7)));
+            } else if (input.startsWith("todo ")) {
+                addTask(new Todo(input.substring(5)));
+            } else if (input.startsWith("deadline ")) {
+                String[] parts = input.substring(9).split(" by ");
+                addTask(new Deadline(parts[0], parts[1]));
+            } else if (input.startsWith("event ")) {
+                String[] parts = input.substring(6).split(" from | to ");
+                addTask(new Event(parts[0], parts[1], parts[2]));
             } else {
-                addTask(input);
+                System.out.println("Sorry, I am not sure what that means.");
             }
         }
     }
 
-    public static void addTask(String input) {
-        Task task = new Task(input);
+    public static void addTask(Task task) {
         tasks.add(task);
-        System.out.println("added: " + input);
+        System.out.println("Got it! I've added this task.");
+        System.out.println(" " + task);
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
     }
 
     public static void printTasks() {
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < tasks.size(); i++) {
-            Task task = tasks.get(i);
-            System.out.println((i + 1) + ".[" + task.getStatusIcon() + "] " + task.description);
+            System.out.println((i + 1) + "." + tasks.get(i));
         }
     }
 
@@ -46,7 +54,7 @@ public class Emily {
             Task task = tasks.get(taskNumber - 1);
             task.markAsDone();
             System.out.println("Nice! I've marked this task as done: ");
-            System.out.println("  [X] " + task.description);
+            System.out.println(" " + task);
         }
     }
 
@@ -55,7 +63,7 @@ public class Emily {
             Task task = tasks.get(taskNumber - 1);
             task.unmarkAsDone();
             System.out.println("Ok, I've unmarked this task as not done yet:");
-            System.out.println("  [ ] " + task.description);
+            System.out.println(" " + task);
         }
     }
 }
